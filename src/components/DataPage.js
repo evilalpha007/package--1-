751df1c0e7761d6c0b6b7b882e6d45468fd79557
thunder-source <majhipraditya@gmail.com>
2023-03-10 16:57:22 +0530

    added match api

--- src/components/DataPage.js
+++ src/components/DataPage.js
@@ -1,32 +1,34 @@
-import React, { useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { onChangeAsideActive } from "../store/features/asideSlice";
 import { useGetCricketMatchesQuery } from "../store/services/mainApi";
 import { onChangeDataActive } from "../store/features/dataSlice";
 
 const DataPage = () => {
+  const navigate = useNavigate();
   const aside = useSelector((state) => state.asideState.aside);
   const dispatch = useDispatch();
 
   const { data, error, isFetching } = useGetCricketMatchesQuery();
-  console.log(data);
   if (error) return <>Error</>;
   if (isFetching) return <> Loading... </>;
+  console.log(data);
 
   return (
     <div>
-      <div className='main_div button-8 mt-3'>
-        <table className='table'>
+      <div className="main_div button-8 mt-3">
+        <table className="table">
           <tbody>
             {data.map((ele, i) => {
               return (
                 <tr key={i}>
-                  <td className='d-flex fsm remove_border hover_effect justify-content-between '>
+                  <td className="d-flex fsm remove_border hover_effect justify-content-between ">
                     <NavLink
-                      to=''
-                      className='flex-1'
+                      to=""
+                      className="flex-1"
                       onClick={() => {
+                        // navigate(`/match/${data.gameId}`);
                         dispatch(onChangeAsideActive("EventAllMatch"));
                         dispatch(
                           onChangeDataActive({
@@ -34,11 +36,12 @@ const DataPage = () => {
                             data: ele,
                           })
                         );
-                      }}>
-                      <strong className='mx-2 fsm'>⭐</strong>
+                      }}
+                    >
+                      <strong className="mx-2 fsm">⭐</strong>
                       {ele?.eventName.split("/")[0]}
                     </NavLink>
-                    <div className=''>
+                    <div className="">
                       {ele.inPlay ? (
                         <small>{ele?.eventName.split("/")[1]}</small>
                       ) : (
@@ -46,22 +49,22 @@ const DataPage = () => {
                       )}
                     </div>
                   </td>
-                  <td className='bluecolor w-63'>
+                  <td className="bluecolor w-63">
                     <strong>-</strong>
                   </td>
-                  <td className='w-63 redcolor text-center'>
+                  <td className="w-63 redcolor text-center">
                     <strong>-</strong>
                   </td>
-                  <td className='w-63 bluecolor'>
+                  <td className="w-63 bluecolor">
                     <strong>-</strong>
                   </td>
-                  <td className='w-63 redcolor text-center'>
+                  <td className="w-63 redcolor text-center">
                     <strong>-</strong>
                   </td>
-                  <td className='w-63 bluecolor'>
+                  <td className="w-63 bluecolor">
                     <strong>-</strong>
                   </td>
-                  <td className='w-63 redcolor text-center'>
+                  <td className="w-63 redcolor text-center">
                     <strong>-</strong>
                   </td>
                 </tr>
